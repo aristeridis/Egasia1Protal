1522f55bd9eb3802b9777cecd5992fa15f3e9502
Alexandros <aristeridis@hotmail.gr>
2022-12-06 16:53:00 +0200

    final

--- src/com/unipi/aristeridis/Main.java
+++ src/com/unipi/aristeridis/Main.java
@@ -6,22 +6,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myInput = new Scanner( System.in );
-        int a,b;
-        System.out.print( "Dose arithmo Ypologiston gia paragwgi: " );
+        Scanner myInput = new Scanner(System.in);
+        int a, b;
+        System.out.print("Dose arithmo Ypologiston gia paragwgi: ");
         a = myInput.nextInt();
-        System.out.print( "Dose arithmo pelaton pou tha anameinoun: " );
+        System.out.print("Dose arithmo pelaton pou tha anameinoun: ");
         b = myInput.nextInt();
         Random rand = new Random();
-        int euros_ypologiston=a;
-        if (euros_ypologiston<=a && euros_ypologiston <=b){
-            int tyxaio = rand.nextInt(euros_ypologiston);
-    }
+        int euros_ypologiston = a;
+        int tyxaioPC = 0;
+        int tyxaioLaptop=0;
+        if (euros_ypologiston <= a && euros_ypologiston <= b) {
+            tyxaioPC = rand.nextInt(euros_ypologiston);
+        }
+        tyxaioLaptop=tyxaioPC-euros_ypologiston;
         Katastima kat1 = Katastima.INSTANCE.getInstance();
         System.out.println(kat1.getInfo());
         kat1.setInfo("New info");
         System.out.println(kat1.getInfo());
         EnimerosiYpologiston observable = new EnimerosiYpologiston();
         DiforopoiisiYpologiston observer = new DiforopoiisiYpologiston();
-}
+        PCFactory pcf1 = new PCFactory();
+        PC pc1=new PC();
+        Laptop lpt1=new Laptop();
+        for( int c=0;c<=tyxaioPC;c++){
+            pcf1.create("PC");
+           pc1.getYpologistis();
+        }
+        for (int c=0;c<=tyxaioLaptop;c++){
+            pcf1.create("Laptop");
+            lpt1.getYpologistis();
+
+        }
+    }
 }
